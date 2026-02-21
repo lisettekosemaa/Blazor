c8b3febd92bcad4cfc1174f0c915bf480c01c056
Lisette Kosemaa <lisettekosemaa@gmail.com>
2026-02-21 17:49:40 +0200

    Lisasin Todo andmebaasi ja parandasin paar viga.

--- Soft/Todo/Server/Data/Migrations/20260221144208_v.21.02.26.Initial.Designer.cs
+++ Soft/Todo/Server/Data/Migrations/20260221144208_v.21.02.26.Initial.Designer.cs
@@ -4,6 +4,7 @@ using Abc.Soft.Todo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Abc.Soft.Todo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260221144208_v.21.02.26.Initial")]
+    partial class v210226Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Soft/Todo/Server/Data/Migrations/20260221144208_v.21.02.26.Initial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Abc.Soft.Todo.Migrations
+{
+    /// <inheritdoc />
+    public partial class v210226Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
