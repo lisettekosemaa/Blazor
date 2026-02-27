952df2cbd5bc712f242ba092accb74ce8ef81056
Lisette Kosemaa <lisettekosemaa@gmail.com>
2026-02-27 02:16:55 +0200

    Lõpetasin Movies MC'i juhendi järgi.

--- Soft/Movies/Server/Data/Migrations/20260227000313_v.27.02.26-validation.Designer.cs
+++ Soft/Movies/Server/Data/Migrations/20260227000313_v.27.02.26-validation.Designer.cs
@@ -4,6 +4,7 @@ using Abc.Soft.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Abc.Soft.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260227000313_v.27.02.26-validation")]
+    partial class v270226validation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
