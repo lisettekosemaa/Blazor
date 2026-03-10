ab1b2f1989df51b113f326b1d712ea3630be48a4
Lisette Kosemaa <lisettekosemaa@gmail.com>
2026-03-10 23:15:29 +0200

    Lisasin Clone klassi ja CloneTests klassi testidega.

---
+++ Code/Aids/Clone.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace Abc.Aids
+{
+    public static class Clone
+    {
+        public static TClass Object<TClass>(TClass obj)
+            where TClass : class, new() => (TClass)ClonedObject(obj);
+
+        private const BindingFlags PublicInstance = BindingFlags.Public | BindingFlags.Instance;
+        private static object ClonedObject(object obj)
+        {
+            if (obj == null) return null;
+            var t = obj.GetType();
+            var o = Activator.CreateInstance(t);
+            var props = t.GetProperties(PublicInstance);
+            Copy(obj, o, props);
+            return o;
+        }
+        public static void Copy(object from, object to, PropertyInfo[] props)
+        {
+            foreach (var p in props)
+            {
+                if (!p.CanRead || !p.CanWrite) continue;
+                var v = p.GetValue(from);
+                if (v != null && IsClass(p))
+                    v = ClonedObject(v);
+                p.SetValue(to, v);
+            }
+        }
+        private static bool IsClass(PropertyInfo p) 
+            => p.PropertyType.IsClass && p.PropertyType != typeof(string);
+    }
+}
