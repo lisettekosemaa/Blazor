a7f29563b36e055474f4535edf34122bf5b2dc65
Lisette Kosemaa <lisettekosemaa@gmail.com>
2026-03-10 21:00:23 +0200

    Lisasin MovieTests klassi Movie testid ja TestAids klassi abistava meetodi IsProperty. Loeng 5 - 2. osa.

--- Tests/Tests.Aids/TestAids.cs
+++ Tests/Tests.Aids/TestAids.cs
@@ -13,4 +13,16 @@ public abstract class TestAids<TClass> where TClass : class, new ()
         => Abc.Aids.GetType.PropertyNames<TClass>(PublicDeclared);
     protected static IEnumerable<string> GetMethods()
         => Abc.Aids.GetType.MethodNames<TClass>(PublicDeclared, false);
+
+    protected void IsProperty<T>(string name)
+    {
+        var p = typeof(TClass).GetProperty(name);
+        Assert.IsNotNull(p, NoProperty(name));
+        Assert.AreEqual(typeof(T), p.PropertyType, WrongType<T>(name, p));
+    }
+    private static string WrongType<T>(string name, PropertyInfo p) 
+        => $"Property '{name}' in class '{typeof(TClass).Name}' is of " +
+           $"type '{p.PropertyType.Name}' instead of '{typeof(T).Name}'.";
+    private static string NoProperty(string name) 
+        => $"Class '{typeof(TClass).Name}' does not have a public property named '{name}'.";
 }
--- Tests/Tests.Data/MovieTests.cs
+++ Tests/Tests.Data/MovieTests.cs
@@ -3,5 +3,12 @@ using Abc.Tests.Aids;
 
 namespace Abc.Tests.Data
 {
-    [TestClass] public sealed class MovieTests : BaseTests<Movie>;
+    [TestClass] public sealed class MovieTests : BaseTests<Movie>
+    {
+        [TestMethod] public void IdTest() => IsProperty<int>(nameof(Movie.Id));
+        [TestMethod] public void TitleTest() => IsProperty<string>(nameof(Movie.Title));
+        [TestMethod] public void ReleaseDateTest() => IsProperty<DateOnly>(nameof(Movie.ReleaseDate));
+        [TestMethod] public void GenreTest() => IsProperty<string>(nameof(Movie.Genre));
+        [TestMethod] public void PriceTest() => IsProperty<decimal>(nameof(Movie.Price));
+    }
 }
